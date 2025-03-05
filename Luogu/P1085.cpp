ec1a21cf886d278cd5de25c181d1d82c11ef1f68
Lithium Valproate <minaki@matsu.rip>
2025-03-05 19:33:09 +0800

    lg3

--- Luogu/P1085.cpp
+++ Luogu/P1085.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a, b, s = 0;
-    int ans = s;
+    int a[8], b[8], ans = 0;
+    a[0] = b[0] = 0;
     for (int i = 1; i < 8; i++) {
-        cin >> a >> b;
-        if (a + b > s && a + b > 7) {
-            s = a + b;
+        cin >> a[i]>> b[i];
+        if (a[i] + b[i] > 8 && a[i] + b[i] > a[ans] + b[ans])
             ans = i;
-        }
     }
     cout << ans;
     return 0;
