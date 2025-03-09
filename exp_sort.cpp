8f01c16c4ac079eb2da17f8dc55d82f47ec34265
Lithium Valproate <minaki@matsu.rip>
2025-03-09 21:46:59 +0800

    bubble sort

--- exp_sort.cpp
+++ exp_sort.cpp
@@ -7,8 +7,6 @@
 using namespace std;
 
 vector<int> counting_sort(const vector<int>& vec) {
-    if (vec.empty())
-        return {};
     int minVal = *min_element(vec.begin(), vec.end());
     int maxVal = *max_element(vec.begin(), vec.end());
     int range = maxVal - minVal + 1;
@@ -24,45 +22,64 @@ vector<int> counting_sort(const vector<int>& vec) {
 }
 
 vector<int> selection_sort(const vector<int>& vec) {
-    if (vec.empty())
-        return {};
     vector<int> output = vec;
     for (int i = 0; i < output.size() - 1; i++) {
         int tmp = i;
-        for (int j = i + 1; j < output.size(); j++)
+        for (int j = i + 1; j < output.size(); j++) {
             if (output[j] < output[tmp])
                 tmp = j;
+        }
         swap(output[i], output[tmp]);
     }
     return output;
 }
 
+vector<int> bubble_sort(const vector<int>& vec) {
+    vector<int> output = vec;
+    for (int i = 0; i < output.size() - 1; i++)
+        for (int j = 0; j < output.size() - i - 1; j++)
+            if (output[j] > output[j + 1]) {
+                int tmp = output[j];
+                output[j] = output[j + 1];
+                output[j + 1] = tmp;
+            }
+    return output;
+}
+
 int main() {
-    cout << "Please enter numbers to sort:" << endl
-         << "Please ends in any character not belongs to numbers" << endl;
+    cout << "Please enter numbers to sort:\n"
+         << "Please ends in any character not belongs to numbers\n";
     int tmpIn;
     vector<int> vecIn;
     while (cin >> tmpIn)
         vecIn.push_back(tmpIn);
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cout << "Please switch which sort you want to use:" << endl
-         << "1. Counting Sort" << endl
-         << "2. Selection Sort" << endl;
+    cout << "Please switch which sort you want to use:\n"
+         << "1. Counting Sort\n"
+         << "2. Selection Sort\n"
+         << "3. Bubble Sort\n";
     int s;
-    vector<int> ans;
     cin >> s;
+    vector<int> ans;
     switch (s) {
     case 1:
         ans = counting_sort(vecIn);
+        break;
     case 2:
         ans = selection_sort(vecIn);
+        break;
+    case 3:
+        ans = bubble_sort(vecIn);
+        break;
     default:
-        for (int i = 0; i < ans.size(); i++)
-            cout << ans[i] << " ";
-        cout << endl;
+        cout << "Invalid option. Using Counting Sort by default.\n";
+        ans = counting_sort(vecIn);
         break;
     }
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << " ";
+    cout << endl;
     system("pause");
     return 0;
 }
