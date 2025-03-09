03d7e60d0df9d4ff70a8cd4417e650fa4c6c0337
Lithium Valproate <minaki@matsu.rip>
2025-03-09 20:33:40 +0800

    selection sort

--- exp_sort.cpp
+++ exp_sort.cpp
@@ -6,35 +6,6 @@
 #include <vector>
 using namespace std;
 
-vector<int> counting_sort(const vector<int>&);
-
-int main() {
-    cout << "Please enter numbers to sort:" << endl
-         << "Please ends in any character not belongs to numbers" << endl;
-    int tmpIn;
-    vector<int> vecIn;
-    while (cin >> tmpIn)
-        vecIn.push_back(tmpIn);
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cout << "Please switch which sort you want to use:" << endl
-         << "1. Counting Sort." << endl;
-    int s;
-    vector<int> ans;
-    cin >> s;
-    switch (s) {
-    case 1:
-        ans = counting_sort(vecIn);
-    default:
-        for (int i = 0; i < ans.size(); i++)
-            cout << ans[i] << " ";
-        cout << endl;
-        break;
-    }
-    system("pause");
-    return 0;
-}
-
 vector<int> counting_sort(const vector<int>& vec) {
     if (vec.empty())
         return {};
@@ -50,4 +21,48 @@ vector<int> counting_sort(const vector<int>& vec) {
         for (int j = 0; j < count[i]; j++)
             output.push_back(i + minVal);
     return output;
+}
+
+vector<int> selection_sort(const vector<int>& vec) {
+    if (vec.empty())
+        return {};
+    vector<int> output = vec;
+    for (int i = 0; i < output.size() - 1; i++) {
+        int tmp = i;
+        for (int j = i + 1; j < output.size(); j++)
+            if (output[j] < output[tmp])
+                tmp = j;
+        swap(output[i], output[tmp]);
+    }
+    return output;
+}
+
+int main() {
+    cout << "Please enter numbers to sort:" << endl
+         << "Please ends in any character not belongs to numbers" << endl;
+    int tmpIn;
+    vector<int> vecIn;
+    while (cin >> tmpIn)
+        vecIn.push_back(tmpIn);
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Please switch which sort you want to use:" << endl
+         << "1. Counting Sort" << endl
+         << "2. Selection Sort" << endl;
+    int s;
+    vector<int> ans;
+    cin >> s;
+    switch (s) {
+    case 1:
+        ans = counting_sort(vecIn);
+    case 2:
+        ans = selection_sort(vecIn);
+    default:
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << endl;
+        break;
+    }
+    system("pause");
+    return 0;
 }
