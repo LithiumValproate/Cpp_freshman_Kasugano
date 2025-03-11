5b516f905e2278b02c6961e83480844997972c45
Lithium Valproate <minaki@matsu.rip>
2025-03-11 23:05:42 +0800

    TG-5_fix

--- XJU/Cpp_T_5-1.cpp
+++ XJU/Cpp_T_5-1.cpp
@@ -1,9 +1,9 @@
 #include <array>
 #include <iostream>
 using namespace std;
-array<int, 2> max_func(int a[], int n) {
-    array<int, 2> ans = {a[0], 1}; 
-    for (int i = 1; i < n; i++) {
+array<int, 2> max_func(int a[]) {
+    array<int, 2> ans = {a[0], 1};
+    for (int i = 1; i < 10; i++) {
         if (a[i] > ans[0]) {
             ans[0] = a[i];
             ans[1] = i + 1;
@@ -12,12 +12,10 @@ array<int, 2> max_func(int a[], int n) {
     return ans;
 }
 int main() {
-    int n;
-    cin >> n;
-    int a[n]; 
-    for (int i = 0; i < n; i++)
+    int a[10];
+    for (int i = 0; i < 10; i++)
         cin >> a[i];
-    array<int, 2> ans = max_func(a, n);
+    array<int, 2> ans = max_func(a);
     printf("Max is %d, number %d in the array.\n", ans[0], ans[1]);
     system("pause");
     return 0;
